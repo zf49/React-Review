49d532486099f4df779edd9075c0b8fc65f66c26
王治方 <you@example.com>
2022-05-17 00:08:14 +0800

    css style and {expression}

--- day1/my-app/src/01-base/04-component_style.js
+++ day1/my-app/src/01-base/04-component_style.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
 
-
+import './css/01-index.css' // importing css moudle which is supported by webpack 
 
 
 
 
 export default class App extends Component {
     render() {
+        var myname = "Chris"
+        var obj = {
+            background:"yellow",
+            fontSize:"10em"
+        }
+
+
         return (
             <div>
-                app
+                {10 + 20}-{myname}
+                <hr></hr>
+                {10 > 20?'aaa':'bbb'}
+
+                <hr></hr>
+
+                <div style={obj}>11111111111111</div>
+                <div style={{background:"red",fontSize:"10em"}}>22222222222222</div>
+
+                <div className="active">3333333333333</div>
+
+                <div id="">practice</div>
+
+                <label htmlFor="username">username</label>
+                <input type="text" id="username"></input>
+
             </div>
         )
     }
