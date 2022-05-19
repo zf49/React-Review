9075954640a989018a2ad7d05fdc858889f8fd29
王治方 <you@example.com>
2022-05-20 00:17:53 +0800

    delete element

--- day1/my-app/src/01-base/10-practice-todolist.js
+++ day1/my-app/src/01-base/10-practice-todolist.js
@@ -5,6 +5,10 @@ export default class App extends Component {
 
     inputValue = React.createRef()
     
+    liValue = React.createRef()
+
+
+
     state = {
         content:[]
     }
@@ -15,7 +19,6 @@ export default class App extends Component {
         var newNum = this.inputValue.current.value
 
         let copyList = [...this.state.content]
-        // let copyList = newNum.slice();
 
         copyList.push(newNum)
 
@@ -23,6 +26,30 @@ export default class App extends Component {
             content:copyList
         })
     }
+
+
+
+        delete(index){
+
+            let copyList = [...this.state.content]
+            
+            // let newList = this.state.content.slice() also can use this method to copy the arrary
+
+
+            copyList.splice(index,1)
+
+            // splice(index, howmany,item1..itemx)
+            // index: whcih position add/delete the element, if use negtive num -> which means from the end of array
+            // howmany: how many element do i need to delete, if it is 0, it will not delete nay element
+
+
+
+            this.setState({
+                content:copyList
+            })
+
+        }
+
 
     render() {
         return (
@@ -39,7 +66,11 @@ export default class App extends Component {
 
                 <div>
                     <ul>{     
-                       this.state.content.map((item,index)=>{return <li key={index}>{item}<button>delete</button></li>})
+                       this.state.content.map((item,index)=>{return <li key={index}>{item}<button onClick={
+                           ()=>{
+                                this.delete(index)
+                           }  
+                       }>delete</button></li>})
                     }
                     </ul>
                 </div>
