03f623235c3fb5d311d29a2b3c3b89d379b13be5
王治方 <you@example.com>
2022-05-21 22:56:52 +0800

    axios

--- day1/my-app/src/01-base/component/Cinema.js
+++ day1/my-app/src/01-base/component/Cinema.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
 
+import axios from 'axios'
+
+
+
 export default class Cinema extends Component {
+
+    constructor(){
+        super()
+
+        // request data
+
+        // axios.get("https://m.maizuo.com/gateway?cityId=110100&ticketFlag=1&k=9286549").then(res=>{console.log(res)}).catch(err=>{console.log(err)})
+        
+        axios({
+            url:"https://m.maizuo.com/gateway?cityId=110100&ticketFlag=1&k=9286549",
+            method:'get',
+            headers:{
+                'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.2.0","e":"1653143769202670916763649","bc":"110100"}',
+                'X-Host': 'mall.film-ticket.cinema.list'
+            }
+        }).then(res=>{console.log(res.data.data.cinemas)})
+
+
+
+    }
+
+
+
     render() {
         return (
             <div>
