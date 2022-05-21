711bd47299ef78ec02aedb94050438313664971a
王治方 <you@example.com>
2022-05-22 00:17:32 +0800

    cinema list

--- day1/my-app/src/01-base/component/Cinema.js
+++ day1/my-app/src/01-base/component/Cinema.js
@@ -7,12 +7,19 @@ import axios from 'axios'
 export default class Cinema extends Component {
 
     constructor(){
+        
+
         super()
+
+
+        this.state={
+            cinemaList:[]
+        }
+
 
         // request data
 
         // axios.get("https://m.maizuo.com/gateway?cityId=110100&ticketFlag=1&k=9286549").then(res=>{console.log(res)}).catch(err=>{console.log(err)})
-        
         axios({
             url:"https://m.maizuo.com/gateway?cityId=110100&ticketFlag=1&k=9286549",
             method:'get',
@@ -20,18 +27,39 @@ export default class Cinema extends Component {
                 'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.2.0","e":"1653143769202670916763649","bc":"110100"}',
                 'X-Host': 'mall.film-ticket.cinema.list'
             }
-        }).then(res=>{console.log(res.data.data.cinemas)})
+        }).then(res=>{
 
+           this.setState({
+               cinemaList:res.data.data.cinemas
+           })
 
-
+        })
     }
+
+
+    
+
+
+       
 
 
 
     render() {
         return (
             <div>
-              Cinema Component  
+                
+                {
+                    this.state.cinemaList.map(item=> 
+                               <dl>
+                                   <dt>{item.name}</dt>
+                                   <dt>{item.address}</dt>
+                                   <hr>
+                                   </hr>
+                                </dl>
+                    )
+                
+                }
+            
             </div>
         )
     }
