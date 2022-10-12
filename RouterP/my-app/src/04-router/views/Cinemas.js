34d0db9b33c97317b5474d42e9cc8ae569d52eda
zf49 <zwan686@aucklanduni.ac.nz>
2022-10-12 18:32:26 +1300

    fixed detail

--- RouterP/my-app/src/04-router/views/Cinemas.js
+++ RouterP/my-app/src/04-router/views/Cinemas.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Cinemas() {
     return (
         <div>
-            Cinemas
+            Cinemas-component
         </div>
     )
 }
--- RouterP/my-app/src/04-router/views/Detail.js
+++ RouterP/my-app/src/04-router/views/Detail.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 
-
-
 export default function Detail(props) {
     console.log(props.match.params.chrisId,"利用Id拿后端数据 ------动态路由")
 
