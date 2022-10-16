eafabc0ce5b2d62e962e5f266d7b3c9fe33d5f73
zf49 <zwan686@aucklanduni.ac.nz>
2022-10-16 17:53:39 +1300

    principle of redux

--- RouterP/my-app/src/05-redux/redux/store.js
+++ RouterP/my-app/src/05-redux/redux/store.js
@@ -23,4 +23,37 @@ const reducer = (preState={
 
 const store = createStore(reducer);
 
+function createChirsStore(reducer){
+    let list = []
+    let state = reducer(undefined,{})
+
+
+    function subscribe(callback){
+        list.push(callback)
+    }
+
+    function dispatch(action){
+        state = reducer(state,action)
+        for(let i in list){
+            list[i] && list[i]()
+        }
+    }
+
+    function getState(){
+        return state
+    }
+
+    return{
+        subscribe,
+        dispatch,
+        getState
+    }
+
+}
+
+
+
+
+
+
 export default store
