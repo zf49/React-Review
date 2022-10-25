56c4cbc7afc4852f7f9bf5c328b30d6ac79f9a44
zf49 <zwan686@aucklanduni.ac.nz>
2022-10-25 13:16:20 +1300

    mobx

--- RouterP/my-app/src/10-mobx/App.js
+++ RouterP/my-app/src/10-mobx/App.js
@@ -5,6 +5,7 @@ import {observable,autorun} from'mobx'
 // 对于普通类型数据的监听
 // 只要10改变了，它就知道
 let observableNumber = observable.box(10)
+let observableName = observable.box('chris')
 
 // autorun第一次就执行，之后每次改变也会执行
 autorun(()=>{
@@ -14,6 +15,8 @@ autorun(()=>{
 
 setTimeout(()=>{
     observableNumber.set(20)
+    observableName.set('王治方')
+
 },1000)
 
 export default function App() {
