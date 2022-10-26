c39c629acfe2c8bfdb6a7e09c4ae402c050c028b
zf49 <zwan686@aucklanduni.ac.nz>
2022-10-26 15:11:08 +1300

    数组

--- TsStudy/my-app-ts/src/01-Tsintro/02-数组.ts
+++ TsStudy/my-app-ts/src/01-Tsintro/02-数组.ts
@@ -1,3 +1,27 @@
 export default {}
 
+let list:string[] = ['1','2','3','4']
+list.push('5')
+for (let i in list){
+
+    list[i].substring(0,1)
+
+}
+
+
+
+let numArr: number[]= [1,2,3,4,5]
+numArr.push(6)
+
+
+let list3:(number|string)[] = [1,2,3,4,5,'1','2','3','5']
+
+//-----------------------------------
+
+let myList1:Array<String> = ['1','3']
+myList1.push('123')
+
+let myList2:Array<String|number> = ['1','3',123,34,456,7]
+myList2.push(123)
+myList2.push("123")
 
