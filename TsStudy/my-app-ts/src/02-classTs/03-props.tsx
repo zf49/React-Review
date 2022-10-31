56dd37ae324d3936c8b9d5d981c3dc9d28d09b24
zf49 <zwan686@aucklanduni.ac.nz>
2022-10-31 17:07:06 +1300

    props 约束

--- TsStudy/my-app-ts/src/02-classTs/03-props.tsx
+++ TsStudy/my-app-ts/src/02-classTs/03-props.tsx
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React, { Children, Component } from 'react';
 
 class App extends Component {
     render() {
         return (
             <div>
                 123
+                <hr></hr>
+                <Child name={"123"}></Child>
             </div>
         );
     }
 }
 
 export default App;
+
+
+interface IProps{
+    name:string
+}
+
+class Child extends Component<IProps,any> {
+    render() {
+        return (
+            <div>
+                children={this.props.name}
+            </div>
+        );
+    }
+}
+
