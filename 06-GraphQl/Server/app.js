f7a445e6b59a549eeedc34964560650f38b66a32
zf49 <zwan686@aucklanduni.ac.nz>
2022-11-15 18:05:22 +1300

    hello graphql

--- 06-GraphQl/Server/app.js
+++ 06-GraphQl/Server/app.js
@@ -3,9 +3,39 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+let homeRouter = require('./routes/homeRouter')
+let listRouter = require('./routes/listRouter')
+
+
+
+
+
+var schema = buildSchema(`
+  type Query {
+    hello: String,
+    getName:String,
+    getAge:Int
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!111111';
+  },
+  getName:()=>{
+    return 'chris'
+  },
+  getAge:()=>{
+    return 26
+  }
+};
 
 var app = express();
 
@@ -19,8 +49,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/home',homeRouter)
+app.use('/list',listRouter)
+
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
